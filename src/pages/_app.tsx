e8a07245da2bd4ec744150a700705e1d007989ff
Mabonga Samuel <samuel.mabonga@iicstechnologies.com>
2023-05-05 08:51:46 +0300

    First commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,34 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react';
+import type { AppProps } from 'next/app';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+import '../styles/globals.css';
+import createEmotionCache from '@/utility/createEmotionCache';
+import lightThemeOptions from '@/theme/lightThemeOptions';
+
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
+
+const clientSideEmotionCache = createEmotionCache();
+
+const lightTheme = createTheme(lightThemeOptions);
+
+const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
