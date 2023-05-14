9888498dcb06b38df4baba7c08a303007caea64f
HassanBahati <mukisabahati@gmail.com>
2023-05-14 16:45:33 +0300

    chore: fix String to primitive type string - changed String to string

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,40 +3,56 @@ import { connectToMongoDB } from "lib/mongodb";
 import User from "models/user";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { IUser } from "types";
 
 const options: NextAuthOptions = {
   providers: [
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "text",
-        },
-        password: { label: "Password", type: "password" },
-        async authorize(credentials) {
-          await connectToMongoDB().catch((err) => {
-            throw new Error(err);
-          });
-          const user = await User.findOne({
-            email: credentials?.email,
-          }).select("+password");
+      CredentialsProvider({
+          id: "credentials",
+          name: "Credentials",
+          credentials: {
+              email: { label: "Email", type: "text" },
+              password: { label: "Password", type: "password" }
+          },
+          async authorize(credentials) {
+              await connectToMongoDB().catch(err => { throw new Error(err) })
 
-          if (!user) {
-            throw new Error("Invalid credentials");
+              const user = await User.findOne({
+                  email: credentials?.email
+              }).select("+password")
+
+              if (!user) {
+                  throw new Error("Invalid credentials")
+              }
+
+              const isPasswordCorrect = await compare(credentials!.password, user.password)
+
+              if (!isPasswordCorrect) {
+                  throw new Error("Invalid credentials")
+              }
+
+              return user
           }
-
-          const isPasswordCorrect = await compare(
-            credentials!.password,
-            user.password
-          );
-
-          if (!isPasswordCorrect) {
-            throw new Error("Invalid credentials");
-          }
-        },
-      },
-    }),
+      })
   ],
-};
+  pages: {
+      signIn: "/login"
+  },
+  session: {
+      strategy: "jwt"
+  },
+  callbacks: {
+      jwt: async ({ token, user }) => {
+          user && (token.user = user)
+          return token
+      },
+      session: async ({ session, token }) => {
+          const user = token.user as IUser
+          session.user = user
+
+          return session
+      }
+  }
+}
+
+export default NextAuth(options)
