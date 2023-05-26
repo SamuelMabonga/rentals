c8690d5475b21efafab9b67b37753bdead4dfee8
Mabonga Samuel <samuel.mabonga@iicstechnologies.com>
2023-05-26 23:14:43 +0300

    push

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: false,
+  },
   images: {
     domains: ["files.stripe.com", "source.unsplash.com", "images.unsplash.com", "d33wubrfki0l68.cloudfront.net", "v5.airtableusercontent.com", "asset.cloudinary.com", "res.cloudinary.com", "picsum.photos", "fastly.picsum.photos", "https://obs.line-scdn.net"],
   },
